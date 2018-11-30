fc73d61c8c8b8d042b706586d44a84a4380b8c2f
Roman Le Négrate <roman.lenegrate@gmail.com>
2018-11-30 10:17:39 +0100

    rewrite in Crystal

---
+++ main.cr
@@ -0,0 +1,57 @@
+require "yaml"
+
+mapping = Mapping.new
+File.open ENV["HOME"] + "/.config/promptpath.yaml" do |f|
+  mapping.load YAML.parse(f)
+end
+
+case ARGV.size
+when 0
+  mapping.each do |path, name|
+    puts "#{name}\t#{path}"
+  end
+when 1
+  puts mapping.shorten ARGV[0]
+else
+  raise ArgumentError.new "usage: #{File.basename PROGRAM_NAME} [path]"
+end
+
+class Mapping < Hash(String, String)
+  def load(config)
+    fill(config, nil)
+
+    @re = %r{^(#{
+      keys.
+        sort_by { |k| [-k.count('/'), -k.size] }.
+        map { |k| Regex.escape k }.
+        join "|"
+    })(?:/(.+)|$)}
+  end
+
+  private def fill(config, root)
+    config.as_h.each do |key, value|
+      path = key.as_s?
+      path = nil if path == "_"
+
+      abs = [root, path].compact.join("/").gsub("~", ENV.fetch("HOME"))
+      !abs.empty? || raise "empty path"
+
+      if name = value.as_s?
+        self[abs] = name
+      else
+        fill value, abs
+      end
+    end
+  end
+
+  def shorten(path)
+    path =~ @re || return path
+    short = self[$1]
+    rel = $2? || return short
+    "#{color "90"}#{short}/#{color "0"}#{rel}"
+  end
+
+  private def color(attrs)
+    "\e[#{attrs}m"
+  end
+end
